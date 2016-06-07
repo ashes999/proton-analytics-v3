5fac6850bba0ee9adcea0687ec673373ccf0c3eb
ashes999 <ashes999@yahoo.com>
2016-06-07 10:50:56 -0400

    Boilerplate MVC + WebAPI + Forms project

--- ProtonAnalytics.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ProtonAnalytics.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ProtonAnalytics.Api/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ProtonAnalytics.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
