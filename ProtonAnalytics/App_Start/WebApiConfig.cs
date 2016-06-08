f7512eaefa8ad302fafa56ca13dc235d57423666
ashes999 <ashes999@yahoo.com>
2016-06-08 14:20:31 -0400

    Wired up login call to API Fixed forms authenticaton in web app (null principal in context -- forms authenticaton module was removed in web.config) Fixed API routes (prefix is `api`)

--- ProtonAnalytics/App_Start/WebApiConfig.cs
+++ ProtonAnalytics/App_Start/WebApiConfig.cs
@@ -25,7 +25,7 @@ namespace ProtonAnalytics
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "{controller}/{id}",
+                routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ ProtonAnalytics/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using ProtonAnalytics.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ProtonAnalytics.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var request = new RestRequest("Account/Login", Method.POST);
+            request.AddObject(model);
+
+            var client = new RestClient(ConfigurationManager.AppSettings["ApiRootUrl"]);
+            var response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                // TODO: support model.RememberMe
+                FormsAuthentication.SetAuthCookie(model.Email, false);
+                HttpContext.User = new GenericPrincipal(new GenericIdentity(model.Email), null);
+                return Redirect("~/");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+            }
+        }
+    }
+}
