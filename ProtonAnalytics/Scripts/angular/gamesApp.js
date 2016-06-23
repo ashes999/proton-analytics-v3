fd0adad402d95b3d9167acf6d4910c960a64ce96
ashes999 <ashes999@yahoo.com>
2016-06-23 16:21:06 -0400

    GamesList controller in Angular uses $resource

--- ProtonAnalytics/Scripts/angular/gamesApp.js
+++ ProtonAnalytics/Scripts/angular/gamesApp.js
@@ -4,10 +4,7 @@ var gamesApp = angular.module('gamesApp', ['ngResource'])
     .controller("GamesListController", function($resource) {
         var gamesList = this;
 
-        // $resource('data/events/:id', { id: '@id' }).get({ id: 1 });
-
-        gamesList.games = [
-            { "name": "first game" },
-            { "name": "second game" }
-        ];
+        $resource('api/Game/GetAll', {}).query().$promise.then(function (results) {
+            gamesList.games = results;
+        })
     });
